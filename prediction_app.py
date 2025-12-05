01285bef9e7fe0191b09faa56832bae264bc151f
mariammohamed-cell <abusaadmariam@gmail.com>
2025-12-06 00:02:52 +0200

    Update prediction_app.py

--- prediction_app.py
+++ prediction_app.py
@@ -1,6 +1,3 @@
-import warnings
-warnings.filterwarnings('ignore')
-
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -30,8 +27,9 @@ def load_model_and_resources():
 
 model, le, selected_features = load_model_and_resources()
 
-# --- UI Mappings ---
+# --- Mappings ---
 urban_rural_options = {"Urban": 1, "Rural": 2, "Unallocated": 3}
+
 light_mapping = {
     'Daylight: Street light present': 4,
     'Darkness: Street lights present and lit': 3,
@@ -39,36 +37,22 @@ light_mapping = {
     'Darkness: Street lights present but unlit': 1,
     'Darkness: No street lighting': 0
 }
-surface_mapping = {'Dry': 4, 'Wet/Damp': 3, 'Frost/Ice': 2, 'Snow': 1, 'Flood': 0}
+
+surface_mapping = {
+    'Dry': 4,
+    'Wet/Damp': 3,
+    'Frost/Ice': 2,
+    'Snow': 1,
+    'Flood': 0
+}
 
 # --- Streamlit UI ---
 st.set_page_config(page_title="Accident Severity Prediction", layout="centered")
 
 st.markdown(
-    """
-    <style>
-    /* Full page background */
-    .stApp {
-        background-image: url("https://i.ibb.co/3m5Qv9V/sky-road.gif");
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center;
-    }
-    /* Floating cars animation */
-    .car {
-        position: absolute;
-        width: 100px;
-        animation: moveCar 5s linear infinite;
-    }
-    @keyframes moveCar {
-        0% {left: -120px;}
-        100% {left: 100%;}
-    }
-    </style>
-    """, unsafe_allow_html=True
+    "<h1 style='text-align: center; color: #4A90E2;'>Accident Severity Prediction</h1>",
+    unsafe_allow_html=True
 )
-
-st.markdown("<h1 style='text-align: center; color: #FFFFFF;'>Accident Severity Prediction</h1>", unsafe_allow_html=True)
 
 # Inputs
 speed_limit = st.slider("Speed Limit (mph)", 20, 70, 30, step=5)
@@ -84,9 +68,13 @@ input_df = pd.DataFrame({
     "Road_Surface_Conditions": [surface_mapping[selected_surface]],
 })
 
+# Feature engineering
 input_df['Speed_Urban_Rural'] = input_df['Urban_or_Rural_Area'] * input_df['Speed_limit']
-input_df['Light_Road_Interaction'] = input_df['Light_Conditions'] * input_df['Road_Surface_Conditions']
+input_df['Light_Road_Interaction'] = (
+    input_df['Light_Conditions'] * input_df['Road_Surface_Conditions']
+)
 
+# Add missing selected features
 for col in selected_features:
     if col not in input_df.columns:
         input_df[col] = 0
@@ -96,31 +84,25 @@ probs = model.predict_proba(input_df)
 pred = np.argmax(probs, axis=1)
 pred_label = le.inverse_transform(pred)[0]
 
-# --- Display Animated Result ---
+# --- Display Animated Prediction ---
 st.markdown(
     f"""
-    <div style='text-align:center; position:relative; height:300px;'>
-        <!-- Floating cars -->
-        <img src="https://i.ibb.co/7tB0dMx/car-road.gif" class="car" style="top:180px;">
-        <img src="https://i.ibb.co/7tB0dMx/car-road.gif" class="car" style="top:220px; animation-duration:7s;">
-        <!-- Prediction -->
+    <div style='text-align:center; margin-top:40px;'>
         <div style='
-            font-size:30px;
+            font-size:32px;
             font-weight:bold;
-            color:#50E3C2;
-            position:absolute;
-            top:100px;
-            width:100%;
+            color:#4A90E2;
             animation: pulse 1.5s infinite;
         '>{pred_label}</div>
     </div>
 
     <style>
     @keyframes pulse {{
-        0% {{ transform: scale(1); }}
-        50% {{ transform: scale(1.2); color:#FFFFFF; }}
-        100% {{ transform: scale(1); }}
+        0% {{ transform: scale(1); color:#4A90E2; }}
+        50% {{ transform: scale(1.25); color:#50E3C2; }}
+        100% {{ transform: scale(1); color:#4A90E2; }}
     }}
     </style>
-    """, unsafe_allow_html=True
+    """,
+    unsafe_allow_html=True
 )
